b98c243406b70a65f5f5d2196253c562d982ed2b
Oleksandr Yampolskyi <les.yampolskyi@gmail.com>
2023-10-19 01:09:52 +0300

    Step1

---
+++ task/pipelines.py
@@ -0,0 +1,18 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import base64
+
+
+class TaskPipeline:
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        adapter["ip_address"] = base64.b64decode(adapter.get("ip_address")).decode("utf-8")
+        adapter["port"] = int(adapter.get("port"))
+
+        return item
---
+++ task/spiders/proxy_list.py
@@ -0,0 +1,33 @@
+import scrapy
+import re
+
+from task.items import TaskItem
+
+
+class ProxyListSpider(scrapy.Spider):
+    name = "proxy_list"
+    allowed_domains = ["free-proxy.cz"]
+    start_urls = ["http://free-proxy.cz/en/"]
+    custom_settings = {
+        "FEEDS": {
+            "proxy_data.json": {
+                "format": "json",
+                "overwrite": True,
+            }
+        }
+    }
+
+    def parse(self, response):
+        items_rows = response.xpath("//table[@id='proxy_list']/tbody/tr")
+        for row in items_rows:
+           
+            script_tag_string = row.xpath('.//td[1]/script').get()
+            ip_address = re.search(r'Base64.decode\("(.*?)\"\)\)</script>', script_tag_string)
+            if ip_address is not None:
+                task_item = TaskItem()
+                task_item['port'] = row.xpath('.//td[2]/span//text()').get()
+                task_item['ip_address'] = ip_address.group(1)
+
+                yield task_item
+            
+          
